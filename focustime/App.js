d2d557f67f00387c166827d1dd5518feda89a214
mamenDseid <mamelala@gmail.com>
2022-06-10 17:24:25 +0000

    cd

--- focustime/App.js
+++ focustime/App.js
@@ -1,11 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, AsyncStorage } from 'react-native';
+
+import { uuidv4 } from './src/utils/uuid';
+import { Timer } from './src/features/timer/Timer';
+import { Focus } from './src/features/focus/Focus';
+import { FocusHistory } from './src/features/focus/FocusHistory';
 
 export default function App() {
+  const [focusSubject, setFocusSubject] = useState(null);
+  const [focusHistory, setFocusHistory] = useState([]);
+
+  const saveFocusHistory = async () => {
+    try {
+      AsyncStorage.setItem('focusHistory', JSON.stringify(focusHistory));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const loadFocusHistory = async () => {
+    try {
+      const history = await AsyncStorage.getItem('focusHistory');
+
+      if (history && JSON.parse(history).length) {
+        setFocusHistory(JSON.parse(history));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadFocusHistory();
+  }, []);
+
+  useEffect(() => {
+    saveFocusHistory();
+  }, [focusHistory]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      {focusSubject ? (
+        <Timer
+          subject={focusSubject}
+          clearSubject={() => {
+            setFocusHistory([
+              ...focusHistory,
+              { subject: focusSubject, status: 0, key: uuidv4() },
+            ]);
+            setFocusSubject(null);
+          }}
+          onTimerEnd={() => {
+            setFocusHistory([
+              ...focusHistory,
+              { subject: focusSubject, status: 1, key: uuidv4() },
+            ]);
+            setFocusSubject(null);
+          }}
+        />
+      ) : (
+        <View style={styles.focusContainer}>
+          <Focus focusHistory={focusHistory} addSubject={setFocusSubject} />
+          <FocusHistory
+            focusHistory={focusHistory}
+            setFocusHistory={setFocusHistory}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +75,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  focusContainer: { flex: 1, backgroundColor: '#252250' },
 });
